a1bfcb60272617f65fcd67ca473c4f50a6142fd2
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-02-12 23:35:24 +0400

    Changes to be committed:  	modified:   LibraryOfTheWorld/Signin.Designer.cs

--- LibraryOfTheWorld/Signin.Designer.cs
+++ LibraryOfTheWorld/Signin.Designer.cs
@@ -101,7 +101,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Signin";
-            this.Text = "Form1";
+            this.Text = "Library";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
