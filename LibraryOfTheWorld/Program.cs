936564d18b38611b2cef85eb8f84baa1cf06b1aa
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-02-14 18:10:20 +0400

    Changes to be committed:  	modified:   LibraryOfTheWorld/App.config  	modified:   LibraryOfTheWorld/LibraryOfTheWorld.csproj  	modified:   LibraryOfTheWorld/Program.cs  	modified:   LibraryOfTheWorld/Properties/Resources.Designer.cs  	modified:   LibraryOfTheWorld/Properties/Settings.Designer.cs  	deleted:    LibraryOfTheWorld/Signin.Designer.cs  	deleted:    LibraryOfTheWorld/Signin.cs  	deleted:    LibraryOfTheWorld/Signin.resx

--- LibraryOfTheWorld/Program.cs
+++ LibraryOfTheWorld/Program.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryOfTheWorld.Users;
+using System.Xml.Linq;
+using LibraryOfTheWorld;
 
 namespace LibraryOfTheWorld
 {
     internal static class Program
     {
         /// <summary>
+        ///
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Signin());
+            Application.Run(new SginUp());
+
         }
+
     }
 }
